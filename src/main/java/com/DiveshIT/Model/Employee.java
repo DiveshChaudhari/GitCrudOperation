1f83d4e0d1d21c9f64488eaf3d2c8327547fe1fd
Divesh Chaudhari <diveshpchaudhari98@gmail.com>
2022-08-24 16:33:22 +0530

    SBCWR-4: create ServiceI Interface

--- src/main/java/com/DiveshIT/Model/Employee.java
+++ src/main/java/com/DiveshIT/Model/Employee.java
@@ -1,7 +1,56 @@
 package com.DiveshIT.Model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Employee {
 
-	private String name;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private String empname;
+	private String empaddress;
+	private String empemail;
+	private double empsalary;
+	private int empage;
+	public String getEmpname() {
+		return empname;
+	}
+	public void setEmpname(String empname) {
+		this.empname = empname;
+	}
+	public String getEmpaddress() {
+		return empaddress;
+	}
+	public void setEmpaddress(String empaddress) {
+		this.empaddress = empaddress;
+	}
+	public String getEmpemail() {
+		return empemail;
+	}
+	public void setEmpemail(String empemail) {
+		this.empemail = empemail;
+	}
+	public double getEmpsalary() {
+		return empsalary;
+	}
+	public void setEmpsalary(double empsalary) {
+		this.empsalary = empsalary;
+	}
+	public int getEmpage() {
+		return empage;
+	}
+	public void setEmpage(int empage) {
+		this.empage = empage;
+	}
+	@Override
+	public String toString() {
+		return "Employee [empname=" + empname + ", empaddress=" + empaddress + ", empemail=" + empemail + ", empsalary="
+				+ empsalary + ", empage=" + empage + "]";
+	}
+	
+	
+	
 }
---
+++ src/main/java/com/DiveshIT/Service/ServiceImpl.java
@@ -0,0 +1,5 @@
+package com.DiveshIT.Service;
+
+public class ServiceImpl {
+
+}
