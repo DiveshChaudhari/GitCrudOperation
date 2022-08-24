2bf0d1dbd4bc330a16a59e719d4961783dfa0964
Divesh Chaudhari <diveshpchaudhari98@gmail.com>
2022-08-24 15:02:00 +0530

    Project Added to central Repository

---
+++ src/main/java/com/DiveshIT/GitCrudOperationApplication.java
@@ -0,0 +1,13 @@
+package com.DiveshIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitCrudOperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitCrudOperationApplication.class, args);
+	}
+
+}
